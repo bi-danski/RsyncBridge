dc712bea2054207f60102d60b08d4866432e308c
bi-danski <bidanski666@gmail.com>
2026-06-12 22:50:08 +0300

    ...

--- shared/src/commonMain/kotlin/org/me2you/rsyncbridge/ui/components/StatusChip.kt
+++ shared/src/commonMain/kotlin/org/me2you/rsyncbridge/ui/components/StatusChip.kt
@@ -1,24 +1,18 @@
 package org.me2you.rsyncbridge.ui.components
 
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.AssistChipDefaults
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-
-@Composable
-fun ControlChip(
-    label: String,
-    containerColor: Color,
-    labelColor: Color,
-    onClick: () -> Unit
-) {
-    AssistChip(
-        onClick = onClick,
-        label = { Text(text = label) },
-        colors = AssistChipDefaults.assistChipColors(
-            containerColor = containerColor.copy(alpha = 0.15f),
-            labelColor = labelColor
-        )
-    )
-}
+//@Composable
+//fun ControlChip(
+//    label: String,
+//    containerColor: Color,
+//    labelColor: Color,
+//    onClick: () -> Unit
+//) {
+//    AssistChip(
+//        onClick = onClick,
+//        label = { Text(text = label) },
+//        colors = AssistChipDefaults.assistChipColors(
+//            containerColor = containerColor.copy(alpha = 0.15f),
+//            labelColor = labelColor
+//        )
+//    )
+//}
--- shared/src/jvmMain/kotlin/org/me2you/rsyncbridge/screens/DashScreen.kt
+++ shared/src/jvmMain/kotlin/org/me2you/rsyncbridge/screens/DashScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -47,7 +46,6 @@ fun DashScreen(
     val isSshActive by rootViewModel.isSshConnected.collectAsStateWithLifecycle()
     val isIProxyActive by rootViewModel.isProxyConnected.collectAsStateWithLifecycle()
     val listState = rememberLazyListState()
-    val localScope = rememberCoroutineScope()
 
     Box(
         modifier = Modifier
